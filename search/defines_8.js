82ad52ba65298baa3511f3c30bf134a90d485641
stnolting <stnolting@users.noreply.github.com>
2021-02-05 16:54:48 +0000

    deploy: e25378e9ff9fbc1a0cfffb61b37157cd1b88451a

--- search/defines_8.js
+++ search/defines_8.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['neorv32_5farchid_1712',['NEORV32_ARCHID',['../neorv32_8h.html#a9bf34265291c07e2aadeaf59e81768ca',1,'neorv32.h']]],
-  ['num_5fcells_5fx_1713',['NUM_CELLS_X',['../game__of__life_2main_8c.html#a78c5b500f0f057bcaedf6fedeca7c537',1,'main.c']]],
-  ['num_5fcells_5fy_1714',['NUM_CELLS_Y',['../game__of__life_2main_8c.html#aed77778557404b38bebb05b5830368b4',1,'main.c']]],
-  ['num_5ftest_5fcases_1715',['NUM_TEST_CASES',['../bit__manipulation_2main_8c.html#a83792c34853f1d2801b59ad46adc7e1e',1,'main.c']]]
+  ['neorv32_5farchid_1767',['NEORV32_ARCHID',['../neorv32_8h.html#a9bf34265291c07e2aadeaf59e81768ca',1,'neorv32.h']]],
+  ['neorv32_5frte_5fnum_5ftraps_1768',['NEORV32_RTE_NUM_TRAPS',['../neorv32__rte_8h.html#ad14a1ca41df1e9300bfd01aef07dce59',1,'neorv32_rte.h']]],
+  ['num_5fcells_5fx_1769',['NUM_CELLS_X',['../game__of__life_2main_8c.html#a78c5b500f0f057bcaedf6fedeca7c537',1,'main.c']]],
+  ['num_5fcells_5fy_1770',['NUM_CELLS_Y',['../game__of__life_2main_8c.html#aed77778557404b38bebb05b5830368b4',1,'main.c']]],
+  ['num_5ftest_5fcases_1771',['NUM_TEST_CASES',['../bit__manipulation_2main_8c.html#a83792c34853f1d2801b59ad46adc7e1e',1,'main.c']]]
 ];
